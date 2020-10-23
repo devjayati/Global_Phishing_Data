e60bac201dec00a9f61c146246ecc2613b6595a8
Max Harms <maxharms@iu.edu>
2020-10-23 14:41:23 -0400

    should get all columns from extracted data.

--- grab_columns.R
+++ grab_columns.R
@@ -1,6 +1,23 @@
 #load the library dplyr for contains() function
 library(dplyr)
 #store output file into variable correct_csv
-correct_csv <- read.csv("output.csv")
+NZ <- read.csv("NZ.csv")
 #select only columns that contain "correct", and store them into a variable
-select(correct_csv,contains("correct"))
+NZcorrect <- select(NZ,contains("correct"))
+
+
+#Do the same for the US
+US <- read.csv("US.csv")
+UScorrect <- select(US,contains("correct"))
+
+#Do the same for the UK
+UK <- read.csv("UK.csv")
+UKcorrect <- select(UK,contains("correct"))
+
+#Do the same for the CA
+CA <- read.csv("CA.csv")
+CAcorrect <- select(CA,contains("correct"))
+
+#Do the same for the AU
+AU <- read.csv("AU.csv")
+AUcorrect <- select(AU,contains("correct"))
